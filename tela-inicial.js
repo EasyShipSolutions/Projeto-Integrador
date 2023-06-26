a77712be8fcaf6475bfacb4316e620ff4e21c5f1
UilhaDougras <williamdouglassb@hotmail.com>
2023-06-26 20:34:55 -0300

    add java script

---
+++ tela-inicial.js
@@ -0,0 +1,12 @@
+function escondermenu() {
+  let barraLateral = document.querySelector('.barra-lateral');
+  barraLateral.style.display = 'none';
+  
+}
+
+function aparecermenu() {
+  let barraLateral = document.querySelector('.barra-lateral');
+  barraLateral.style.display = 'block'; 
+  
+}
+
